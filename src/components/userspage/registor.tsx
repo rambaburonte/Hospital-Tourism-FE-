4f4f7b15ac0dfe5da39b3969e66236d6721fa2d5
AnilNaga <nagaanil0329@gmail.com>
2025-05-09 13:52:27 +0530

    refresh

--- src/components/userspage/registor.tsx
+++ src/components/userspage/registor.tsx
@@ -42,7 +42,8 @@ export default function RegisterPage() {
    
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
          <Header/>
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+         <div className="sm:mx-auto sm:w-full sm:max-w-[40rem]">
+
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Create your account
         </h2>
